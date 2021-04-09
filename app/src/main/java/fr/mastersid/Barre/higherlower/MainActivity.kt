a90f83ad46d14095c526cfd169e37fb4132bd9fc
BryanBarre <barrebryan289@gmail.com>
2021-04-09 20:12:06 +0200

    tp4 end

--- app/src/main/java/fr/mastersid/Barre/higherlower/MainActivity.kt
+++ app/src/main/java/fr/mastersid/Barre/higherlower/MainActivity.kt
@@ -1,34 +1,36 @@
 package fr.mastersid.Barre.higherlower
 
-import android.graphics.Path
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavGraph
-import androidx.navigation.NavGraphNavigator
-import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
-import fr.mastersid.Barre.higherlower.Settings.SettingsFragmentDirections
 import fr.mastersid.Barre.higherlower.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity () {
     override fun onCreate ( savedInstanceState : Bundle ?) {
         super . onCreate ( savedInstanceState )
+
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView (binding.root)
         setSupportActionBar(binding.toolbar)
+
         val navController = findNavController (R.id.nav_host_fragment )
-        val appBarConfiguration = AppBarConfiguration ( navController . graph )
+        val appBarConfiguration = if ( binding.drawerLayout!=null ) {
+            AppBarConfiguration (navController.graph, binding.drawerLayout  )
+        } else {
+            AppBarConfiguration (navController.graph)
+        }
+
         binding.toolbar.setupWithNavController (navController,appBarConfiguration)
         binding.navView.setupWithNavController (navController)
-
-        binding . navView . setNavigationItemSelectedListener { item ->
+        binding.navView.setNavigationItemSelectedListener { item ->
+            binding.drawerLayout?.closeDrawers ()
             if (item.itemId == R.id.secretNumberFragment) {
-                val action = SettingsFragmentDirections.actionSettingsFragmentToSecretNumberFragment(6, 100)
+                val action = NavGraphDirections.actionGlobalSecretNumberFragment(6, 100)
                 findNavController(R.id.nav_host_fragment).navigate(action)
                 true
             } else {
@@ -37,7 +39,7 @@ class MainActivity : AppCompatActivity () {
         }
     }
     override fun onCreateOptionsMenu ( menu : Menu?): Boolean {
-        menuInflater . inflate (R. menu . toolbar_menu , menu )
+        menuInflater.inflate (R.menu.toolbar_menu,menu )
         return true
     }
     override fun onOptionsItemSelected (item : MenuItem): Boolean {
